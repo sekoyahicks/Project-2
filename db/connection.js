c4fe401d6e2ae0cf1589c1b6ea6b76215f45b67b
Sekoya Hicks <sekoya91@yahoo.com>
2019-04-30 16:40:19 -0400

    deleted comments

--- db/connection.js
+++ db/connection.js
@@ -1,17 +1,4 @@
-// const connectionString = "mongodb://localhost/Project-2";
 
-// //newUrlParser disables the deprication warning
-// mongoose.connect(connectionString, { useNewUrlParser: true }).then(() => {
-//   console.log("connected to mongo at: " + connectionString);
-// });
-
-// mongoose.connection.once("open", function() {
-//   console.log("MOngoose has connected to MongoDB");
-// });
-// module.exports = mongoose;
-
-
-// /
 
 // requirements: import mongoose
 const mongoose = require('mongoose');

22ed9437716594e82078aad7d062d18785aa5512
Sekoya Hicks <sekoya91@yahoo.com>
2019-04-30 16:01:44 -0400

    Added info to connection.js file so project can deploy to Heroku.

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,16 @@
-const mongoose = require("mongoose");
-const connectionString = "mongodb://localhost/Project-2";
 
-//newUrlParser disables the deprication warning
-mongoose.connect(connectionString, { useNewUrlParser: true }).then(() => {
-  console.log("connected to mongo at: " + connectionString);
+// Connect to database
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+  mongoose.connect('mongodb://localhost/Project-2');
+}
+mongoose.connection.on('error', function(err) {
+  console.error('MongoDB connection error: ' + err);
+  process.exit(-1);
+  }
+);
+mongoose.connection.once('open', function() {
+  console.log("Mongoose has connected to MongoDB!");
 });
-
-mongoose.connection.once("open", function() {
-  console.log("MOngoose has connected to MongoDB");
-});
-module.exports = mongoose;

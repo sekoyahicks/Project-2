a57de80dc9c16312668174bc11cf09f758b46fc9
Sekoya Hicks <sekoya91@yahoo.com>
2019-04-30 16:25:09 -0400

    DB is still broken.

--- db/connection.js
+++ db/connection.js
@@ -12,20 +12,24 @@ const mongoose = require("mongoose");
 // module.exports = mongoose;
 
 
+// /
+
+// requirements: import mongoose
+const mongoose = require('mongoose');
 // Connect to database
 if (process.env.MONGODB_URI) {
-  mongoose.connect(process.env.MONGODB_URI);
+  mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 }
 else {
-  mongoose.connect('mongodb://localhost/Project-2');
+  mongoose.connect('mongodb://localhost/Project-2', { useNewUrlParser: true });
 }
-mongoose.connection.on('error', function(err) {
+mongoose.connection.on('error', function (err) {
   console.error('MongoDB connection error: ' + err);
   process.exit(-1);
-  }
+}
 );
-mongoose.connection.once('open', function() {
+mongoose.connection.once('open', function () {
   console.log("Mongoose has connected to MongoDB!");
 });
-
-module.exports = mongoose
+// export your mongoose connection
+module.exports = mongoose;
